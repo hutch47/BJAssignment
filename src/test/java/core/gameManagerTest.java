66bb283c0e75557ca7b6a5819ae2df34015e7ac3
hutch47 <dudeofthedude_321@hotmail.com>
2018-09-18 12:04:56 -0400

    test: Test files created, gameManagerTest methods completed: testPlayerHit(), testEnum(), and testPlayerHitRepeat

--- src/test/java/core/gameManagerTest.java
+++ src/test/java/core/gameManagerTest.java
@@ -3,13 +3,25 @@ package core;
 import junit.framework.TestCase;
 
 public class gameManagerTest extends TestCase {
-	GameManager GM = new GameManager();
 	public void testEnum() {
+		GameManager GM = new GameManager();
 		// Test Jack
 		assertEquals(10, GM.J);
 		// Test Queen
 		assertEquals(10, GM.Q);
 		// Test King
 		assertEquals(10, GM.K);
+	}
+	public void testPlayerHit() {
+		GameManager GM = new GameManager();
+		// Test if player hits successfully
+		GM.playerHit(9);
+		assertEquals(9, GM.playerHandValue);
+	}
+	public void testPlayerHitRepeat() {
+		GameManager GM = new GameManager();
+		GM.playerHit(9);
+		GM.playerHit(8);
+		assertEquals(17, GM.playerHandValue);
 	}
 }
