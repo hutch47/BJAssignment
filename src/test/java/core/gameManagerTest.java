744f490f4869d985f26234f40b3d761892262e2b
hutch47 <dudeofthedude_321@hotmail.com>
2018-09-19 15:36:16 -0400

    test: Finished game manager test functions

--- src/test/java/core/gameManagerTest.java
+++ src/test/java/core/gameManagerTest.java
@@ -26,9 +26,34 @@ public class gameManagerTest extends TestCase {
 	}
 	public void testPlayerTwoAce() {
 		GameManager GM = new GameManager();
-		GM.playerHit(GM.A);
-		GM.playerHit(GM.A);
+		GM.playerDeal("HA", "DA");
 		GM.playerHit(10);
 		assertEquals(17, GM.getPlayerHandValue());
+	}
+	public void testPlayerBlackjack() {
+		GameManager GM = new GameManager();
+		
+		// playerHit will return a 0 if under 21, a -1 if bust, and a 1 if 21
+		// playerDeal will return a 1 if blackjack, 0 otherwise
+		assertEquals(1, GM.playerDeal("SA", "HJ"));
+	}
+	public void testDealerBlackjack() {
+		GameManager GM = new GameManager();
+		
+		assertEquals(1, GM.dealerDeal("DA", "DJ"));
+	}
+	public void testDealerWinBlackJack() {
+		GameManager GM = new GameManager();
+		int p, d;
+		p = GM.playerDeal("DJ", "DQ");
+		d = GM.dealerDeal("HJ", "DA");
+		assertEquals(-1, p - d);
+	}
+	public void testPlayerWinBlackjack() {
+		GameManager GM = new GameManager();
+		int p, d;
+		p = GM.playerDeal("DJ", "DA");
+		d = GM.dealerDeal("HJ", "DQ");
+		assertEquals(1, p - d);
 	}
 }
