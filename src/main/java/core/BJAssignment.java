36102a9bfbe6518e7b5f175fc3e1ea583ef9eedc
hutch47 <dudeofthedude_321@hotmail.com>
2018-09-19 23:10:46 -0400

    resources: added text files to read for testing main: completed file reading functionality misc: added comments

--- src/main/java/core/BJAssignment.java
+++ src/main/java/core/BJAssignment.java
@@ -1,8 +1,14 @@
 package core;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class BJAssignment {
 	
@@ -59,13 +65,13 @@ public class BJAssignment {
 				}
 				System.out.println();
 				if (GM.dealerBJ()) {
-					System.out.println("Dealer Wins!");
+					System.out.println("Dealer Wins! Blackjack!");
 					playing = false;
 					gameWon = true;
 					break;
 				}
 				else if (!GM.dealerBJ() && GM.playerBJ()) {
-					System.out.println("You win!");
+					System.out.println("You win! Blackjack!");
 					playing = false;
 					gameWon = true;
 					break;
@@ -146,7 +152,127 @@ public class BJAssignment {
 			
 			//Game loop (reading)
 			else {
+				gameWon = false;
+				List<String> inst = new ArrayList<>();
+				GM = new GameManager();
 				
+				// Get file name and store each instruction
+				while (true) {
+				System.out.print("Please enter the file name: ");
+				String fName = br.readLine();
+				try (Scanner sc = new Scanner(new File("src/main/resources/"+fName))) {
+					while (sc.hasNext()) {
+						inst.add(sc.next());
+					}
+					break;
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					System.out.println("File not found!");
+				}
+				}
+				
+				// Run the game
+				GM.playerDeal(inst.remove(0), inst.remove(0));
+				GM.dealerDeal(inst.remove(0), inst.remove(0));
+				System.out.print("Your cards are: ");
+				for (int i = 0; i < GM.player.getNumCards(); i++) {
+					System.out.print(GM.player.getCard(i) + " ");
+				}
+				System.out.println();
+				System.out.print("The dealer has these cards shown: XX ");
+				for (int i = 1; i < GM.dealer.getNumCards(); i++) {
+					System.out.print(GM.dealer.getCard(i) + " ");
+				}
+				System.out.println();
+				if (GM.dealerBJ()) {
+					System.out.println("Dealer Wins! Blackjack!");
+					playing = false;
+					gameWon = true;
+					break;
+				}
+				else if (!GM.dealerBJ() && GM.playerBJ()) {
+					System.out.println("You win! BlackJack!");
+					playing = false;
+					gameWon = true;
+					break;
+				}
+				
+				// If no initial win, continue game
+				while (!gameWon) {
+					dealerWinning = GM.dealerWin();
+					while (inst.size() > 0) {
+						
+						
+						// If hitting
+						if (inst.get(0).equals("H")) {
+							inst.remove(0);
+							GM.playerHit(inst.remove(0));
+							if (GM.playerBust()) {
+								System.out.println("You Bust!");
+								dealerWinning = true;
+								gameWon = true;
+								break;
+							}
+							System.out.println();
+							System.out.print("Your cards are: ");
+							for (int i = 0; i < GM.player.getNumCards(); i++) {
+								System.out.print(GM.player.getCard(i) + " ");
+							}
+							System.out.println();
+							System.out.print("The dealer has these cards shown: XX ");
+							for (int i = 1; i < GM.dealer.getNumCards(); i++) {
+								System.out.print(GM.dealer.getCard(i) + " ");
+							}
+							System.out.println();
+							if (inst.size() == 0)
+								break;
+						}
+						
+						// If staying
+						else if (inst.get(0).equals("S")) {
+							inst.remove(0);
+							System.out.println("The player is staying...");
+							System.out.print("The dealer has these cards shown: XX ");
+							for (int i = 1; i < GM.dealer.getNumCards(); i++) {
+								System.out.print(GM.dealer.getCard(i) + " ");
+							}
+							System.out.println();
+							while (GM.dealerHittable() && inst.size() > 0) {
+								System.out.println("The dealer is hitting...");
+								GM.dealerHit(inst.remove(0));
+								System.out.print("The dealer has these cards shown: XX ");
+								for (int i = 1; i < GM.dealer.getNumCards(); i++) {
+									System.out.print(GM.dealer.getCard(i) + " ");
+								}
+								System.out.println();
+							}
+							if (GM.dealerBust()) {
+								System.out.println("The dealer busts");
+							}
+							else
+								System.out.println("The dealer stands");
+							System.out.print("The dealer has these cards shown: ");
+							for (int i = 0; i < GM.dealer.getNumCards(); i++) {
+								System.out.print(GM.dealer.getCard(i) + " ");
+							}
+							System.out.println();
+							if (GM.dealerWin()) {
+								dealerWinning = true;
+							}
+							break;
+							
+						}
+					}
+					gameWon = true;
+					if (dealerWinning) {
+						System.out.println("Dealer wins!");
+						break;
+					}
+					else {
+						System.out.println("You win!");
+						break;
+					}
+				}
 			}
 		}
 		
--- src/main/java/core/Royal.java
+++ src/main/java/core/Royal.java
@@ -1,5 +1,8 @@
 package core;
 
+// Method of custom enum taken from JavaHowTo.blogspot.com
+// URL: http://javahowto.blogspot.com/2008/04/java-enum-examples.html
+
 public enum Royal {
 	J(10), Q(10), K(10);
 	private int id;
